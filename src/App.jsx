1d30c140305ffad31f41168d991e26f01f8e023a
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-22 15:04:13 +0200

    loading german nouns

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import './App.scss'
+import './App.scss';
 import { useContext } from 'react';
 import { AppContext } from './AppContext';
 
 function App() {
-  const { state, dispatch} = useContext(AppContext);
+    const { state, dispatch } = useContext(AppContext);
 
-  return (
-    <div className="App">
-    <h1>useContext / useReducer Site</h1>
+    return (
+        <div className="App">
+            <h1>useContext / useReducer Site</h1>
 
-      <p>Count = { state.count }</p>
+            <h2>Count</h2>
+            <p>Count = {state.count}</p>
 
-      <div className="buttonArea">
-        <button onClick={() => dispatch('decreaseCount')}>-</button>
-        <button onClick={() => dispatch('increaseCount')}>+</button>
-      </div>
-    </div>
-  )
+            <div className="buttonArea">
+                <button onClick={() => dispatch({ type: 'decreaseCount' })}>
+                    -
+                </button>
+                <button onClick={() => dispatch({ type: 'increaseCount' })}>
+                    +
+                </button>
+            </div>
+        
+        <h2>German Nouns</h2>
+        <p>There are {state.germanNouns.length} German nouns.</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
